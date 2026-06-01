024ec2ac4f2d018a047bdf6cacc9019e81802f98
Zovogtag <escherr993@gmail.com>
2026-06-01 10:13:17 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,9 +25,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made by Zovo",
+        name: "HELLO",
         // Activity type number (0 = Playing).
-        type: 0, 
+        type: 2, 
       },
     ],
   },
